28fc088e47c7c4a3a75c381878a9dc6916177a1c
kmwamasali <kmwamasali@gmail.com>
2021-06-02 01:39:22 +0300

    Bugfix: Return array list

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,14 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   const results = [
     { name: 'kevin', city: 'kampala' },
     { name: 'Jonny', city: 'Stockholm' },
     { name: 'Jonas', city: 'Berlin' }
   ]
-  req.body.results = results;
-  next();
+  res.send(results);
 });
 
 module.exports = router;
